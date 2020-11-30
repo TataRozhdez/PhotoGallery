5fb5c93b3779c77fb23f73215fac9996cdf27600
Tata Rozhdez <tanya.zhukova.95@gmail.com>
2020-11-30 14:09:23 +0200

    change randar favorite page

--- src/pages/Favorite/Favorite.js
+++ src/pages/Favorite/Favorite.js
@@ -1,8 +1,10 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect, useContext } from 'react'
+import { Link } from 'react-router-dom'
 import { Navbar } from '../../components/Navbar/Navbar'
 import ImgContext from '../../context/imgContext'
 import minusImg from '../../img/checked.svg'
+import downloadImg from '../../img/download.svg'
 import leftArrow from '../../img/left.svg'
 import rightArrow from '../../img/next.svg'
 import './Favorite.scss'
@@ -13,6 +15,11 @@ export const Favorite = () => {
   const [renderLarge, setRenderLarge] = useState(0)
 
   const { addToFavorite } = imgContext
+
+  const handleStorage = (item) => {
+    addToFavorite(item)
+    setFavorites(JSON.parse(localStorage.getItem('photoGallery')))
+  }
 
   useEffect(() => {
     setFavorites(JSON.parse(localStorage.getItem('photoGallery')))
@@ -50,42 +57,47 @@ export const Favorite = () => {
     <>
       <Navbar step2='disabled' />
       <div className='favorite-screen'>
-        <h3>Favorite images ♡</h3>
-        {favorites ? (
+        <h3>Favorite images ♡ </h3>
+        {favorites.length > 0 && <p>Total: {favorites.length} items</p>}
+        {favorites.length > 0 ? (
           <div className='favorite-grid'>
             {favorites.map((i, index) => (
               <div key={i.id} className={seteClassName(index)}>
-                <a href={i.download_url} target='_balnk'>
-                  <img
-                    className='favorite-image'
-                    src={i.download_url}
-                    alt={i.author}
-                  />
-                </a>
-
                 <img
-                  className='to-favorite'
-                  src={minusImg}
-                  alt='Add to favorite'
-                  onClick={() => addToFavorite(i)}
+                  className='favorite-image'
+                  src={i.download_url}
+                  alt={i.author}
+                  onClick={() => setRenderLarge(index)}
                 />
+                <div className='favorite-item_footer'>
+                  <a
+                    className='favorite_download'
+                    href={i.download_url}
+                    target='_balnk'
+                  >
+                    <img src={downloadImg} alt='Download' />
+                  </a>
+                  <button onClick={() => handleStorage(i)}>
+                    <img
+                      className='to-favorite'
+                      src={minusImg}
+                      alt='Add to favorite'
+                    />
+                  </button>
+                </div>
               </div>
             ))}
-            <img
-              className='favorite-next'
-              src={rightArrow}
-              alt='Next'
-              onClick={nextRender}
-            />
-            <img
-              className='favorite-prev'
-              src={leftArrow}
-              alt='Previous'
-              onClick={prevRender}
-            />
+            <button className='favorite-next' onClick={nextRender}>
+              <img src={rightArrow} alt='Next' />
+            </button>
+            <button className='favorite-prev' onClick={prevRender}>
+              <img src={leftArrow} alt='Previous' />
+            </button>
           </div>
         ) : (
-          <h4>Add any images to your favorites list...</h4>
+          <p>
+            <Link to='/'>Add any images to your favorites list...</Link>
+          </p>
         )}
       </div>
     </>
